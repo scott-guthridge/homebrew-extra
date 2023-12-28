971b6df7184f8561a1a473fcbff1fccf32c8b9d9
Scott Guthridge <59371690+scott-guthridge@users.noreply.github.com>
2023-12-28 02:01:04 +0000

    libvna: add 0.3.9 bottle.

--- Formula/lib/libvna.rb
+++ Formula/lib/libvna.rb
@@ -5,6 +5,12 @@ class Libvna < Formula
   sha256 "b6c8cec3bb77b4d2bc1bf4ef63cdb383d1bbd7eaa260334aa77c7c4b658b86a7"
   license "GPL-3.0-or-later"
   head "https://github.com/scott-guthridge/libvna.git", branch: "master"
+
+  bottle do
+    root_url "https://github.com/scott-guthridge/homebrew-extra/releases/download/libvna-0.3.9"
+    sha256 cellar: :any,                 ventura:      "6c3fd60903b2af29184cd13872470d4310248217e6006163c91764bf0ffc7cdd"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "3a2d316c4cba22ad02950f451c2e47add55cb00323e8c712978487e185dae732"
+  end
   depends_on "libyaml"
 
   def install
